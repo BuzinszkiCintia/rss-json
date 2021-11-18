dc4c3db19b15d8f18ffc0d1dd2ead39be3d5456e
BuzinszkiCintia <buzinszki.cintia@gmail.com>
2021-11-18 11:12:36 +0100

    Update 3

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+function trimDOM(str) {
+    let parsel = new DOMParser(); //példányosítás, a dom parser az egy konstruktor; beolvastuk, feldolgozza és csinál egy új dom tree-t ez a DOM parsel konst. 
+    let doc = parsel.parseFromString(str, "text/html"); 
+    let txt = doc.body.innerText;
+    return txt.substring(0, 255) + "...";
+}
+
 function readNews() {
-    fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss")
+    fetch("rss.json")
     .then((r)=> r.json()) //ezzel adom meg hogy json-t hozzon létre
     .then(renderNews);
 
@@ -14,18 +21,20 @@ function renderNews(news) {
 
 function renderItem(item) {//it hozzuk létre azt amit látni akarok kint az odlalon
     let box = document.createElement("div"); //dobozt hoztuk létre
+    box.className = "article";
     let title = document.createElement("h1"); //címet hoztuk létre
     let desc = document.createElement("p"); // táblázat
     let link = document.createElement("a");
 
     title.innerText = item.title; //egy adott item-nek kiolvassuk most a title mezejét és belerakjuk a title-be
+    
     link.innerText = item.link; 
     link.href = item.link;
-    desc.innerText = item.summary.substring(0,120) + "..."; // egy itemnek a summary tulajdonságát adjuk vissza (szöveget) + substring-el levágjuk
+    desc.innerText = trimDOM (item.summary);; // egy itemnek a summary tulajdonságát adjuk vissza (szöveget) + substring-el levágjuk (ez változott)
     
     box.append(title, desc); // most a dobozba beleírtuk az előző két sorba lévő szöveget
     document.getElementById("rss").append(box);
 
 }
 //event listener 
-window.addEventListener("load", readNews); // 
+window.addEventListener("load", readNews); 
